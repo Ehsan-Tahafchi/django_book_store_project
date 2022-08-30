1b7e5e29bf95c448573bc150071dd95ce8215fc8
EhsanTahafchi <ehsantahafchi@gmail.com>
2022-08-31 02:21:14 +0430

    create custom user with forms and signup

---
+++ accounts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'age', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('home/', views.HomePageView.as_view(), name='HomePageView')
+
+]
