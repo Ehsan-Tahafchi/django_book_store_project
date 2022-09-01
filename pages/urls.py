6565e65e80fb4f8e51f5ed1c03f15fe67a62dbb6
EhsanTahafchi <ehsantahafchi@gmail.com>
2022-09-01 11:42:29 +0430

    add tests

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home/', views.HomePageView.as_view(), name='HomePageView')
+    path('', views.HomePageView.as_view(), name='HomePageView')
 
 ]
