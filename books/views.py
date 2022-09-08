0f5490f4d147d2a9458da4c07de1cbd66ebe17ff
EhsanTahafchi <ehsantahafchi@gmail.com>
2022-09-08 11:55:33 +0430

    add media to project and add pagination to book list view

--- books/views.py
+++ books/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 4
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 
@@ -16,13 +17,13 @@ class BookDetailView(generic.DetailView):
 
 class BookCreateView(generic.CreateView):
     model = Book
-    fields = ['title', 'author', 'description', 'price']
+    fields = ['title', 'author', 'description', 'price', 'cover']
     template_name = 'books/book_create.html'
 
 
 class BookUpdateView(generic.UpdateView):
     model = Book
-    fields = ['title', 'author', 'description']
+    fields = ['title', 'author', 'description', 'price', 'cover']
     template_name = 'books/book_update.html'
 
 
--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[str(BASE_DIR.joinpath('static'))]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
